1019b65627b260ec8d036b84b1bd2b5d5182899c
bilalbentoumi <bilalbentoumi@gmail.com>
2022-10-07 17:33:50 +0100

    Init app ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Logo from './components/Logo'
+import Select from './components/Select'
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+
+      <div className="navbar border-b">
+        <div className="container py-4 sm:py-10">
+          <Logo />
+        </div>
+      </div>
+
+      <div className="main">
+
+        <div className="container py-4 sm:py-16">
+
+          <h2 className="text-slate-700 text-3xl font-semibold">Currency Converter</h2>
+          <span className="text-slate-500 text-xs font-medium">Today, 2022-10-07</span>
+
+          <div className="flex items-center gap-10 mt-10">
+
+            <Select label="From" className="flex-1" />
+
+            <button className="flex justify-center items-center w-12 h-12 bg-[#3589e7] hover:ring-8 hover:ring-blue-100 rounded-full duration-75">
+              <svg className="fill-current text-white" width="22" height="22" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
+                <path d="M847.9 592H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h605.2L612.9 851c-4.1 5.2-.4 13 6.3 13h72.5c4.9 0 9.5-2.2 12.6-6.1l168.8-214.1c16.5-21 1.6-51.8-25.2-51.8zM872 356H266.8l144.3-183c4.1-5.2.4-13-6.3-13h-72.5c-4.9 0-9.5 2.2-12.6 6.1L150.9 380.2c-16.5 21-1.6 51.8 25.1 51.8h696c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8z" fill="inherit" />
+              </svg>
+            </button>
+
+            <Select label="To" className="flex-1" />
+
+          </div>
+
+          <div className="flex items-center gap-10 mt-10">
+
+            <input type="text" className="w-full text-6xl" placeholder="1000" />
+
+            <input type="text" className="w-full text-6xl" placeholder="1000" />
+
+          </div>
+
+        </div>
+        
+      </div>
+
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Select.tsx
@@ -0,0 +1,66 @@
+import { useRef, useState } from 'react'
+import useOnClickOutside from '../hooks/useOnClickOutside'
+
+const options = [
+  { value: 'usd', text: 'USD' },
+  { value: 'eur', text: 'EUR' },
+  { value: 'dzd', text: 'DZD' },
+]
+
+const Select = ({ label, className }: any) => {
+
+  const ref = useRef<any>()
+
+  const [open, setOpen] = useState(false)
+  const [selectedOption, setSelectedOption] = useState<any>(options[0])
+
+  const select = (option: any) => {
+
+    setSelectedOption(option)
+    setOpen(false)
+  }
+
+  useOnClickOutside(ref, () => setOpen(false))
+
+  return (     
+      
+    <div className={`relative mt-1 ${className}`}>
+      
+      <button onClick={() => setOpen(open => (!open))} type="button" className="relative w-full cursor-default border-b bg-white py-4 text-left hover:border-blue-200 focus:border-blue-600 sm:text-sm">
+        
+        <span className="block text-xs font-medium text-slate-400">{label}</span>
+        <span className="block text-xl">{selectedOption.text}</span>
+        
+        <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+          <svg className="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fillRule="evenodd" d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" clipRule="evenodd" />
+          </svg>
+        </span>
+
+      </button>
+
+      {open && <ul ref={ref} className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+
+        {options.map((option) => (
+          <li key={option.value} onClick={() => {select(option)}} className="hover:bg-gray-100 text-gray-900 relative cursor-default select-none py-2 pl-3 pr-9">
+            
+            <div className="flex items-center">
+              <span className="font-normal block truncate">{option.text}</span>
+            </div>
+
+            {option.value == selectedOption?.value && <span className="text-indigo-600 absolute inset-y-0 right-0 flex items-center pr-4">
+              <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path fillRule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clipRule="evenodd" />
+              </svg>
+            </span>}
+
+          </li>
+        ))}
+
+      </ul>}
+
+    </div>
+  )
+}
+  
+export default Select

1f919268cd57673e2ca155b7d366b8bfc4c3ea64
bilalbentoumi <bilalbentoumi@gmail.com>
2022-10-14 15:16:49 +0100

    Add menu on mobile biew

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react'
 import Logo from './Logo'
 
 const NavBar = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false)
 
   return (
     <div className="navbar border-b bg-white">
@@ -23,14 +26,32 @@ const NavBar = () => {
           </li>
         </ul>
 
-        <button className="block sm:hidden py-8">
-          <svg width="22" viewBox="0 0 23 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <rect width="23" height="3" rx="1.5" fill="black"></rect>
-              <rect y="7" width="23" height="3" rx="1.5" fill="black"></rect>
-              <rect y="14" width="23" height="3" rx="1.5" fill="black"></rect>
-          </svg>
-        </button>
+        <div className="relative block sm:hidden ">
 
+          <button className="py-8" onClick={() => setMenuOpen(menuOpen => !menuOpen)}>
+            <svg width="22" viewBox="0 0 23 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="23" height="3" rx="1.5" fill="black"></rect>
+                <rect y="7" width="23" height="3" rx="1.5" fill="black"></rect>
+                <rect y="14" width="23" height="3" rx="1.5" fill="black"></rect>
+            </svg>
+          </button>
+
+          {menuOpen && <ul className="block w-[200px] py-2 sm:hidden mt-2 absolute right-0 bg-white shadow-md rounded z-10">
+            <li>
+              <a href="/" className="block text-indigo-600 text-sm font-medium px-6 py-2">Home</a>
+            </li>
+            <li>
+              <a href="/" className="block text-slate-500 hover:text-slate-800 text-sm font-medium px-6 py-2">Bank currency</a>
+            </li>
+            <li>
+              <a href="/" className="block text-slate-500 hover:text-slate-800 text-sm font-medium px-6 py-2">Bank reviews</a>
+            </li>
+            <li>
+              <a href="/" className="block text-slate-500 hover:text-slate-800 text-sm font-medium px-6 py-2">Currency info</a>
+            </li>
+          </ul>}
+
+        </div>
 
       </div>
     </div>
